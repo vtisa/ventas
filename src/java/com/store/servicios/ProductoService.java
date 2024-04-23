99fb6bba87669ae751eccb6da21ee05260bdc0a1
isai-valqui <135535849+isai-valqui@users.noreply.github.com>
2024-04-22 23:16:48 -0500

    firts commit

--- src/java/com/store/servicios/ProductoService.java
+++ src/java/com/store/servicios/ProductoService.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.store.servicios;
 
 import com.store.dao.ProductoDAO;
@@ -9,10 +6,6 @@ import com.store.modelo.Producto;
 import java.sql.Connection;
 import java.util.List;
 
-/**
- *
- * @author deyve
- */
 public class ProductoService {
     private final ProductoDAO productoDAO;
 
